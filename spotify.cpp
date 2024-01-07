b4cc48db10afd22bbb4c11c0e96c9633cde17197
wettortoise <rmclaughlin6666@gmail.com>
2024-01-07 21:10:13 +0000

    gets tokens

--- spotify.cpp
+++ spotify.cpp
@@ -19,8 +19,6 @@
 
     scopes: "user-library-read user-read-playback-state"
     headers: {"Authorization": "Bearer {access_token]}","Content-Type": "application/json "}
-
-    "https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri=redirect_uri&scope={scopes.replace(' ','%20')}" => gives code for you to use to get the refresh token which you will refresh to get the access token
 */
 #include "header.h"
 #include <iostream>
@@ -55,15 +53,25 @@ int write_token(std::string token_type, std::string value_wanted){
 } 
 
 
+std::string redirect_uri = "http://localhost:8888/callback";
 
-// get CODE= value from giving application permissions 
+// get CODE= value from giving application permissions, will send you to the spotify site to give permission if you already gave permission it will just go straight to the redirect uri  
 void first(){
-    
+    std::string scopes = replace_spaces("user-library-read user-read-playback-state");
+    std::cout <<  "https://accounts.spotify.com/authorize?client_id=" << get_token("client_id")  << "&response_type=code&redirect_uri=" << redirect_uri << "&scope=" <<  scopes << "\n";
 }
 
-// use code to get refresh token 
-void second(){
-
+// use code that was received after giving permission to app to get refresh token 
+void get_tokens(std::string code){
+    std::stringstream params;
+    params<< "grant_type=authorization_code&code=" << code << "&redirect_uri=" << redirect_uri << "&client_id=" << get_token("client_id") << "&client_secret="<< get_token("client_secret");
+    nlohmann::json result =  post_request("https://accounts.spotify.com/api/token",params.str());
+    if (result.find("error") == result.end()){ // if error isnt found 
+        // {access_token:"",refresh_token:""}
+        write_token("access_token",result["access_token"]);
+        write_token("refresh_token",result["refresh_token"]);
+    }
+    //std::cout << result << "\n";
 }
 
 // use refresh token to get access token 
@@ -87,10 +95,13 @@ void get_liked_albums(){
 
 
 int main(){
-    std::cout << "access: " << write_token("access_token","d") << "\n";
-    std::cout << "refresh: " << write_token("refresh_token","c") << "\n";
-    std::cout << "client_id: " << write_token("client_id","b") << "\n";
-    std::cout << "client_secret: " << write_token("client_secret","a") << "\n";
+
+    // if there isnt any refresh token then 
+    first();
+
+    //then get refresh using code in url  
+    second("AQBIt-V5NQ_e7aO_n4YAdNjAUg4ON-ZOZxbO4alWjnv65KxWKI9zglhKXmL8K0JNIAvm0l1cKG0uCa3hy1i-Wwj_9FU02MoufkbHxO9OmLVouVqOQAbmoiHs_wPuiFobuNgL6cHxoblSA09N6P6Ybvmmxkm5domYqDaJ7-jgtboGQmel922geZHC54o4ENB1m52_Fx2wmlydzjhW-6oKFpaKcnSAwpSf1b5rlpn6nA");
+
 }
 
 
