db9172965c2cdab2de0e79663ab2a92e8cdfe342
wettortoise <rmclaughlin6666@gmail.com>
2024-01-23 22:27:08 +0000

    .

--- header.h
+++ header.h
@@ -1,7 +1,13 @@
 #include <string>
 #include <nlohmann/json.hpp>
-
+#include <variant>
 
 std::string replace_spaces(const std::string& input);
-nlohmann::json get_request(std::string url);
+nlohmann::json get_request(std::string url,bool header = false);
 nlohmann::json post_request(std::string url,std::string paramaters);
+std::string get_token(std::string token_type);
+bool is_in_vec_dics(std::vector<std::unordered_map<std::string, std::string>> list_of_dicts,std::string key, std::string value);
+nlohmann::json is_in_json(nlohmann::json list_of_dicts,std::string key, std::string value);
+
+std::vector<std::unordered_map<std::string, std::string>> get_unique_albums();
+void refresh_access();
